7041024d9ba8ea796d223419cee3323a2767b33a
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-13 00:01:55 +0100

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -78,9 +78,10 @@ class Enemy(pygame.sprite.Sprite):
             # self.rect.y = 
 
 # if you have time at the end, think about how you would use a heart image in the game instead of a rectangle for hearts: 
-# do the same for enemies. 
+# do the same for an enemy picture for the enemy. 
 # see https://www.geeksforgeeks.org/python-display-images-with-pygame/ and https://www.pygame.org/docs/ref/image.html#pygame.image.load
-
+# you may use a tool like https://www.pixilart.com/draw to create your own artwork, if you would prefer
+            
 # here is a main to test if your code is working. It should change from start to paused when the space bar is pressed. 
 # When you press space again, it goes to display your hearts and enemies on screen. You may continually press the space bar to respawn enemies. 
 def main():
