40f53c78884e5c5d9a74327241cf304b205a9ca9
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-10-13 00:02:35 +0100

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -46,6 +46,71 @@ class Bullet(pygame.sprite.Sprite): # see exercise 2
     def __init__(self, x, y): 
         super().__init__()
     def update(self): 
-      
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # create a rectangle for the player similar to the enemy class, but using the BLUE hex code constant (see exercise1.py)
+        self.velocity_y = 0
+        self.on_ground = False
+        self.health = # set how many lives you want your player to have 
+        self.hit_timer = 0   # A timer to manage the cooldown after the player gets hit.
+        self.bullets_fired = 0 # Count how many bullets the player has fired in the current level.
+        self.last_shot_time = 0 # Record the time when the last bullet was shot to manage shooting cooldown.
+        self.shoot_cooldown = 500  # Define the cooldown time for shooting bullets in milliseconds.
+
+    def update(self):
+        self.handle_input()
+        # Call methods to apply gravity, and check if on ground here
+       
+        # Manage hit cooldown
+        if self.hit_timer > 0:
+            self.hit_timer -= 1  # Decrement the timer
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        current_time = pygame.time.get_ticks()
+
+        # Movement controls (Arrow keys)
+        if keys[pygame.K_LEFT]:  # Move left
+            self.rect.x -= PLAYER_SPEED # decrement the playerspeed 
+        if keys[pygame.K_RIGHT]:  # Move right
+            # increment the PLAYER_SPEED
+        if keys[pygame.K_UP]:  # Jump
+            if self.on_ground:  # Ensure the player can only jump if on the ground
+                self.velocity_y = -JUMP_STRENGTH # change the velocity in the y direction by the JUMP_STRENGTH constant
+                # set on_ground to false 
+
+        # Shooting mechanism with cooldown
+        if keys[pygame.K_d] and self.bullets_fired < MAX_BULLETS: # if we have shot less than the max bullets 
+            if current_time - self.last_shot_time > self.shoot_cooldown: # if we are out of the bullet cooldown period
+                bullet = Bullet(self.rect.centerx, self.rect.top)
+                all_sprites.add(bullet) # add bullet to all_sprites (list of all sprites)
+                # add bullet to bullets() 
+                # increment bullets_fired += 1  
+                # set last_shot_time to current_time to measure cooldown 
+
+    def apply_gravity(self):
+       # velocity_y is incremented by the GRAVITY constant 
+       # rect.y is incremented by velocity_y
+       
+    def check_ground(self):
+        # Reset the player's position if they're on the ground
+       # if the rect.bottom is greater than the height: 
+       # the rect.bottom is reset to the HEIGHT, the velocity_y is reset to 0 
+       # and on_ground is set to True 
+
+        # Check for collision with platforms
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect) and self.velocity_y >= 0:
+               # rect.bottom is equal to platform.rect.top (the bottom of the rectangle is equal to the top of the platform 
+                self.rect.bottom = platform.rect.top
+               # set velocity_y to 0
+               # set on_ground to True 
+                break  # Only check against the first platform colliding with
+               
+# if you have finished this week's exercise, think about creating a new enemy that can attack the player in a different way
+# again, you can add a different picture for the Player rect: # see https://www.geeksforgeeks.org/python-display-images-with-pygame/ and https://www.pygame.org/docs/ref/image.html#pygame.image.load
+# you may use a tool like https://www.pixilart.com/draw to create your own artwork, if you would prefer
 
 pygame.quit()
